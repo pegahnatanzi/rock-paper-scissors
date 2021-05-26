f91919d484f655be9bbf363bec8a911ff114b5b4
Pegah Natanzi <64666171+pegahnatanzi@users.noreply.github.com>
2021-05-25 20:21:14 -0400

    Capture user input

--- game.py
+++ game.py
@@ -1,5 +1,10 @@
 print("Rock, Paper, Scissors, Shoot!")
 
-print(10)
+#print(10)
+#print(10, 99, "My message", "another message")
 
-print(10, 99, "My message", "another message")
+
+user_choice = input("Please choose one of 'rock', 'paper', 'scissors;: ")
+
+#print(user_choice)
+print("USER CHOICE", user_choice)
