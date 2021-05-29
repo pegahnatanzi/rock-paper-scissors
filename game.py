62d029c436617e6ae7b937eb91c616a4ddf992e5
Pegah Natanzi <64666171+pegahnatanzi@users.noreply.github.com>
2021-05-29 17:49:05 -0400

    Updated game.py with another valid method

--- game.py
+++ game.py
@@ -86,28 +86,47 @@ if user_choice == computer_choice:
     print("It;s a tie, try again!")
 
 # Now, we'll use an elif clause. The way the computer evaluates this is: if the first if condition isn't true, it's going to go to the next elif clause, starting from left to right 
-# (Code adapated from Reid in Slack)
+# (Code adapated from Reid Whelan in Slack)
 
-if user_choice == "rock":
-    if computer_choice == "rock":
-        print("IT'S A TIE")
-    elif computer_choice == "paper":
-        print("OH, THE COMPUTER WON...")
-    elif computer_choice == "scissors":
-        print("YOU WON! CONGRATS!")
-elif user_choice == "paper":
-    if computer_choice == "rock":
-        print("YOU WON! CONGRATS!")
-    elif computer_choice == "paper":
-        print("IT'S A TIE")
-    elif computer_choice == "scissors":
-        print("OH, THE COMPUTER WON...")
-elif user_choice == "scissors":
-    if computer_choice == "rock":
-        print("OH, THE COMPUTER WON...")
-    elif computer_choice == "paper":
-        print("YOU WON! CONGRATS!")
-    elif computer_choice == "scissors":
-        print("IT'S A TIE")
+elif (user_choice == "scissors") and (computer_choice == "paper"):
+        print("Scissors cuts paper, you win!")
+elif (user_choice == "rock") and (computer_choice == "scissors"):
+        print("rock smashes scissors, you win!")
+elif (user_choice == "paper") and (computer_choice == "rock"):
+        print("Paper covers rock, you win!")
+#COMPUTER WINNING SCENARIOS
+elif (user_choice == "scissors") and (computer_choice == "rock"):
+        print("rock smashes scissors, you lose!")
+elif (user_choice == "rock") and (computer_choice == "paper"):
+        print("paper covers rock, you lose!")
+elif (user_choice == "paper") and (computer_choice == "scissors"):
+        print("Scissors cuts paper, you lose!")
+
 
 print("Thanks for playing. Please play again!")
+
+
+
+# Another method (adapted from JanZielonka in Slack): 
+
+# if user_choice == "rock":
+#     if computer_choice == "rock":
+#         print("IT'S A TIE")
+#     elif computer_choice == "paper":
+#         print("OH, THE COMPUTER WON...")
+#     elif computer_choice == "scissors":
+#         print("YOU WON! CONGRATS!")
+# elif user_choice == "paper":
+#     if computer_choice == "rock":
+#         print("YOU WON! CONGRATS!")
+#     elif computer_choice == "paper":
+#         print("IT'S A TIE")
+#     elif computer_choice == "scissors":
+#         print("OH, THE COMPUTER WON...")
+# elif user_choice == "scissors":
+#     if computer_choice == "rock":
+#         print("OH, THE COMPUTER WON...")
+#     elif computer_choice == "paper":
+#         print("YOU WON! CONGRATS!")
+#     elif computer_choice == "scissors":
+#         print("IT'S A TIE")
